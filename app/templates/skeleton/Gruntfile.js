99068ab8ff9f37a4340dd40b97916616135b381b
Randika Hapugoda <hrandika@hotmail.com>
2015-04-02 17:17:50 +0530

    Addded 'grunt-connect-proxy' support in 'grunt serve'

--- app/templates/skeleton/Gruntfile.js
+++ app/templates/skeleton/Gruntfile.js
@@ -8,241 +8,331 @@ var pkg = require('./package.json');
 //this method is used to create a set of inclusive patterns for all subdirectories
 //skipping node_modules, bower_components, static, and any .dirs
 //This enables users to create any directory structure they desire.
-var createFolderGlobs = function(fileTypePatterns) {
-  fileTypePatterns = Array.isArray(fileTypePatterns) ? fileTypePatterns : [fileTypePatterns];
-  var ignore = ['node_modules','bower_components','static','temp'];
-  var fs = require('fs');
-  return fs.readdirSync(process.cwd())
-          .map(function(file){
+var createFolderGlobs = function (fileTypePatterns) {
+    fileTypePatterns = Array.isArray(fileTypePatterns) ? fileTypePatterns : [fileTypePatterns];
+    var ignore = ['node_modules', 'bower_components', 'static', 'temp'];
+    var fs = require('fs');
+    return fs.readdirSync(process.cwd())
+        .map(function (file) {
             if (ignore.indexOf(file) !== -1 ||
                 file.indexOf('.') === 0 ||
                 !fs.lstatSync(file).isDirectory()) {
-              return null;
+                return null;
             } else {
-              return fileTypePatterns.map(function(pattern) {
-                return file + '/**/' + pattern;
-              });
+                return fileTypePatterns.map(function (pattern) {
+                    return file + '/**/' + pattern;
+                });
             }
-          })
-          .filter(function(patterns){
+        })
+        .filter(function (patterns) {
             return patterns;
-          })
-          .concat(fileTypePatterns);
+        })
+        .concat(fileTypePatterns);
 };
 
 module.exports = function (grunt) {
 
-  // load all grunt tasks
-  require('load-grunt-tasks')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
-  grunt.initConfig({
-    connect: {
-      main: {
-        options: {
-          port: 9001
-        }
-      }
-    },
-    watch: {
-      main: {
-        options: {
-            livereload: true,
-            livereloadOnError: false,
-            spawn: false
+    // Project configuration.
+    grunt.initConfig({
+        // Grunt server Configuration
+        connect: {
+            proxies: [
+                {
+                    context: ['/user', '/logout'],
+                    host: 'localhost',
+                    port: 8080,
+                    https: false,
+                    changeOrigin: true
+                }
+            ],
+            options: {
+                port: 9001,
+                hostname: 'localhost',
+                livereload: 35729
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect, options) {
+                        if (!Array.isArray(options.base)) {
+                            options.base = [options.base];
+                        }
+                        // Setup the proxy
+                        var middlewares = [require('grunt-connect-proxy/lib/utils').proxyRequest];
+                        // Serve static files.
+                        options.base.forEach(function (base) {
+                            middlewares.push(connect.static(base));
+                        });
+                        // Make directory browse-able.
+                        var directory = options.directory || options.base[options.base.length - 1];
+                        middlewares.push(connect.directory(directory));
+                        return middlewares;
+                    }
+                }
+            }
         },
-        files: [createFolderGlobs(['*.js','*.less','*.html']),'!_SpecRunner.html','!.grunt'],
-        tasks: [] //all the tasks are run dynamically during the watch event handler
-      }
-    },
-    jshint: {
-      main: {
-        options: {
-            jshintrc: '.jshintrc'
+        // Grunt live reload Configuraions
+        watch: {
+            main: {
+                options: {
+                    livereload: true,
+                    livereloadOnError: false,
+                    spawn: false
+                },
+                files: [createFolderGlobs(['*.js', '*.less', '*.html']), '!_SpecRunner.html', '!.grunt'],
+                tasks: [] //all the tasks are run dynamically during the watch event handler
+            }
         },
-        src: createFolderGlobs('*.js')
-      }
-    },
-    clean: {
-      before:{
-        src:['static','temp']
-      },
-      after: {
-        src:['temp']
-      }
-    },
-    less: {
-      production: {
-        options: {
+        jshint: {
+            main: {
+                options: {
+                    jshintrc: '.jshintrc'
+                },
+                src: createFolderGlobs('*.js')
+            }
         },
-        files: {
-          'temp/app.css': 'app.less'
-        }
-      }
-    },
-    ngtemplates: {
-      main: {
-        options: {
-            module: pkg.name,
-            htmlmin:'<%%= htmlmin.main.options %>'
+        clean: {
+            before: {
+                src: ['static', 'temp']
+            },
+            after: {
+                src: ['temp']
+            }
         },
-        src: [createFolderGlobs('*.html'),'!index.html','!_SpecRunner.html'],
-        dest: 'temp/templates.js'
-      }
-    },
-    copy: {
-      main: {
-        files: [
-          {src: ['img/**'], dest: 'static/'},
-          {src: ['bower_components/font-awesome/fonts/**'], dest: 'static/',filter:'isFile',expand:true},
-          {src: ['bower_components/bootstrap/fonts/**'], dest: 'static/',filter:'isFile',expand:true},
-          {src: ['bower_components/webcomponentsjs/webcomponents.js'],dest: 'static/',filter: 'isFile',expand: true}       
-        ]
-      },
+        less: {
+            production: {
+                options: {},
+                files: {
+                    'temp/app.css': 'app.less'
+                }
+            }
+        },
+        ngtemplates: {
+            main: {
+                options: {
+                    module: pkg.name,
+                    htmlmin: '<%= htmlmin.main.options %>'
+                },
+                src: [createFolderGlobs('*.html'), '!index.html', '!_SpecRunner.html'],
+                dest: 'temp/templates.js'
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        src: ['img/**'],
+                        dest: 'static/'
+                    },
+                    {
+                        src: ['bower_components/font-awesome/fonts/**'],
+                        dest: 'static/',
+                        filter: 'isFile',
+                        expand: true
+                    },
+                    {
+                        src: ['bower_components/bootstrap/fonts/**'],
+                        dest: 'static/',
+                        filter: 'isFile',
+                        expand: true
+                    },
+                    {
+                        src: ['bower_components/webcomponentsjs/webcomponents.js'],
+                        dest: 'static/',
+                        filter: 'isFile',
+                        expand: true
+                    }
+                ]
+            },
             copyApp: {
                 files: [{
                     src: ['static/**'],
                     dest: "../src/main/resources/"
                 }]
             }
-    },
-    dom_munger:{
-      read: {
-        options: {
-          read:[
-            {selector:'script[data-concat!="false"]',attribute:'src',writeto:'appjs'},
-            {selector:'link[rel="stylesheet"][data-concat!="false"]',attribute:'href',writeto:'appcss'}
-          ]
         },
-        src: 'index.html'
-      },
-      update: {
-        options: {
-          remove: ['script[data-remove!="false"]','link[data-remove!="false"]'],
-          append: [
-            {selector:'body',html:'<script src="app.full.min.js"></script>'},
-            {selector:'head',html:'<link rel="stylesheet" href="app.full.min.css">'},
-            {selector:'head',html:'<script src="bower_components/webcomponentsjs/webcomponents.js"></script>'},
-            {selector:'head',html:'<link rel="import" href="elements.vulcanized.html">'}
-          ]
+        dom_munger: {
+            read: {
+                options: {
+                    read: [
+                        {
+                            selector: 'script[data-concat!="false"]',
+                            attribute: 'src',
+                            writeto: 'appjs'
+                        },
+                        {
+                            selector: 'link[rel="stylesheet"][data-concat!="false"]',
+                            attribute: 'href',
+                            writeto: 'appcss'
+                        }
+                    ]
+                },
+                src: 'index.html'
+            },
+            update: {
+                options: {
+                    remove: ['script[data-remove!="false"]', 'link[data-remove!="false"]'],
+                    append: [
+                        {
+                            selector: 'body',
+                            html: '<script src="app.full.min.js"></script>'
+                        },
+                        {
+                            selector: 'head',
+                            html: '<link rel="stylesheet" href="app.full.min.css">'
+                        },
+                        {
+                            selector: 'head',
+                            html: '<script src="bower_components/webcomponentsjs/webcomponents.js"></script>'
+                        },
+                        {
+                            selector: 'head',
+                            html: '<link rel="import" href="elements.vulcanized.html">'
+                        }
+                    ]
+                },
+                src: 'index.html',
+                dest: 'static/index.html'
+            }
         },
-        src:'index.html',
-        dest: 'static/index.html'
-      }
-    },
-    cssmin: {
-      main: {
-        src:['temp/app.css','<%%= dom_munger.data.appcss %>'],
-        dest:'static/app.full.min.css'
-      }
-    },
-    concat: {
-      main: {
-        src: ['<%%= dom_munger.data.appjs %>','<%%= ngtemplates.main.dest %>'],
-        dest: 'temp/app.full.js'
-      }
-    },
-    ngAnnotate: {
-      main: {
-        src:'temp/app.full.js',
-        dest: 'temp/app.full.js'
-      }
-    },
-    uglify: {
-      main: {
-        src: 'temp/app.full.js',
-        dest:'static/app.full.min.js'
-      }
-    },
-    htmlmin: {
-      main: {
-        options: {
-          collapseBooleanAttributes: true,
-          collapseWhitespace: true,
-          removeAttributeQuotes: true,
-          removeComments: true,
-          removeEmptyAttributes: true,
-          removeScriptTypeAttributes: true,
-          removeStyleLinkTypeAttributes: true
+        cssmin: {
+            main: {
+                src: ['temp/app.css', '<%= dom_munger.data.appcss %>'],
+                dest: 'static/app.full.min.css'
+            }
         },
-        files: {
-          'static/index.html': 'static/index.html'
-        }
-      }
-    },
-    vulcanize: {
+        concat: {
+            main: {
+                src: ['<%= dom_munger.data.appjs %>', '<%= ngtemplates.main.dest %>'],
+                dest: 'temp/app.full.js'
+            }
+        },
+        ngAnnotate: {
+            main: {
+                src: 'temp/app.full.js',
+                dest: 'temp/app.full.js'
+            }
+        },
+        uglify: {
+            main: {
+                src: 'temp/app.full.js',
+                dest: 'static/app.full.min.js'
+            }
+        },
+        htmlmin: {
+            main: {
+                options: {
+                    collapseBooleanAttributes: true,
+                    collapseWhitespace: true,
+                    removeAttributeQuotes: true,
+                    removeComments: true,
+                    removeEmptyAttributes: true,
+                    removeScriptTypeAttributes: true,
+                    removeStyleLinkTypeAttributes: true
+                },
+                files: {
+                    'static/index.html': 'static/index.html'
+                }
+            }
+        },
+        vulcanize: {
             default: {
                 options: {
                     strip: true,
                     inline: true
                 },
                 files: {
-                    'static/elements.vulcanized.html': [
-            'elements.html'
-          ]
+                    'static/elements.vulcanized.html': ['elements.html']
                 }
             }
-        },    
-    karma: {
-      options: {
-        frameworks: ['jasmine'],
-        files: [  //this files data is also updated in the watch handler, if updated change there too
-          '<%%= dom_munger.data.appjs %>',
+        },
+        karma: {
+            options: {
+                frameworks: ['jasmine'],
+                files: [ //this files data is also updated in the watch handler, if updated change there too
+          '<%= dom_munger.data.appjs %>',
           'bower_components/angular-mocks/angular-mocks.js',
           createFolderGlobs('*-spec.js')
         ],
-        logLevel:'ERROR',
-        reporters:['mocha'],
-        autoWatch: false, //watching is handled by grunt-contrib-watch
-        singleRun: true
-      },
-      all_tests: {
-        browsers: ['PhantomJS','Chrome','Firefox']
-      },
-      during_watch: {
-        browsers: ['PhantomJS']
-      },
-    }
-  });
+                logLevel: 'ERROR',
+                reporters: ['mocha'],
+                autoWatch: false, //watching is handled by grunt-contrib-watch
+                singleRun: true
+            },
+            all_tests: {
+                browsers: ['PhantomJS', 'Chrome', 'Firefox']
+            },
+            during_watch: {
+                browsers: ['PhantomJS']
+            },
+        }
+    });
 
-  grunt.registerTask('build',['jshint','clean:before','less','dom_munger','ngtemplates','cssmin','concat','ngAnnotate','uglify','copy','vulcanize','htmlmin','clean:after','copy:copyApp']);
-  grunt.registerTask('serve', ['dom_munger:read','jshint','connect', 'watch']);
-  grunt.registerTask('test',['dom_munger:read','karma:all_tests']);
+    grunt.registerTask('build', [
+        'jshint',
+        'clean:before',
+        'less',
+        'dom_munger',
+        'ngtemplates',
+        'cssmin',
+        'concat',
+        'ngAnnotate',
+        'uglify',
+        'copy',
+        'vulcanize',
+        'htmlmin',
+        'clean:after',
+        'copy:copyApp'
+    ]);
 
-  grunt.event.on('watch', function(action, filepath) {
-    //https://github.com/gruntjs/grunt-contrib-watch/issues/156
+    grunt.registerTask('serve', [
+        'dom_munger:read',
+        'configureProxies:server',
+        'jshint',
+        'connect:livereload',
+        'watch'
+    ]);
 
-    var tasksToRun = [];
+    grunt.registerTask('test', [
+        'dom_munger:read',
+        'karma:all_tests'
+    ]);
 
-    if (filepath.lastIndexOf('.js') !== -1 && filepath.lastIndexOf('.js') === filepath.length - 3) {
+    grunt.event.on('watch', function (action, filepath) {
+        //https://github.com/gruntjs/grunt-contrib-watch/issues/156
+        var tasksToRun = [];
 
-      //lint the changed js file
-      grunt.config('jshint.main.src', filepath);
-      tasksToRun.push('jshint');
+        if (filepath.lastIndexOf('.js') !== -1 && filepath.lastIndexOf('.js') === filepath.length - 3) {
 
-      //find the appropriate unit test for the changed file
-      var spec = filepath;
-      if (filepath.lastIndexOf('-spec.js') === -1 || filepath.lastIndexOf('-spec.js') !== filepath.length - 8) {
-        spec = filepath.substring(0,filepath.length - 3) + '-spec.js';
-      }
+            //lint the changed js file
+            grunt.config('jshint.main.src', filepath);
+            tasksToRun.push('jshint');
 
-      //if the spec exists then lets run it
-      if (grunt.file.exists(spec)) {
-        var files = [].concat(grunt.config('dom_munger.data.appjs'));
-        files.push('bower_components/angular-mocks/angular-mocks.js');
-        files.push(spec);
-        grunt.config('karma.options.files', files);
-        tasksToRun.push('karma:during_watch');
-      }
-    }
+            //find the appropriate unit test for the changed file
+            var spec = filepath;
+            if (filepath.lastIndexOf('-spec.js') === -1 || filepath.lastIndexOf('-spec.js') !== filepath.length - 8) {
+                spec = filepath.substring(0, filepath.length - 3) + '-spec.js';
+            }
 
-    //if index.html changed, we need to reread the <script> tags so our next run of karma
-    //will have the correct environment
-    if (filepath === 'index.html') {
-      tasksToRun.push('dom_munger:read');
-    }
+            //if the spec exists then lets run it
+            if (grunt.file.exists(spec)) {
+                var files = [].concat(grunt.config('dom_munger.data.appjs'));
+                files.push('bower_components/angular-mocks/angular-mocks.js');
+                files.push(spec);
+                grunt.config('karma.options.files', files);
+                tasksToRun.push('karma:during_watch');
+            }
+        }
 
-    grunt.config('watch.main.tasks',tasksToRun);
+        //if index.html changed, we need to reread the <script> tags so our next run of karma
+        //will have the correct environment
+        if (filepath === 'index.html') {
+            tasksToRun.push('dom_munger:read');
+        }
 
-  });
+        grunt.config('watch.main.tasks', tasksToRun);
+
+    });
 };
